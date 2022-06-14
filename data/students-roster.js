56b513499f2d68c59274795295dc0b49247483cb
haayd <haydee.sramirez@gmail.com>
2022-06-13 20:47:09 -0700

    complete student roster objects array and export

--- data/students-roster.js
+++ data/students-roster.js
@@ -1,3 +1,11 @@
 const students = [
-  {name:"John Doe", date:"06/13/22", present: true, _idNumber: 123456,}
+  {name:"John Doe", date:"06/13/22", present: true, _idNumber: 123456,},
+  {name:"Jane Doe", date:"06/13/22", present: false, _idNumber: 654321,},
+  {name:"Juan Doe", date:"06/13/22", present: true, _idNumber: 789012,},
+  {name:"Juana Doe", date:"06/13/22", present: false, _idNumber: 210987,}
+
 ]
+
+export {
+  students
+}
